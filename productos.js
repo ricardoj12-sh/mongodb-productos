eb6a133cb48bef22bfeef8a88c2890a5b7477ca4
Ricardo Josué Suero Hernandez <148921582+ricardoj12-sh@users.noreply.github.com>
2024-09-05 22:35:03 -0400

    conectar
    escribe mongod en la terminal y abre otra terminal para usar mongosh y escribe el comando use onlineTienda, para despues escribir comandos del archivo

---
+++ productos.js
@@ -0,0 +1,37 @@
+// Insertar productos en la colección "productos"
+db.productos.insertMany([
+    {
+      nombre: "Camiseta",
+      precio: 19.99,
+      categoria: "Ropa",
+      stock: 100
+    },
+    {
+      nombre: "Pantalones",
+      precio: 39.99,
+      categoria: "Ropa",
+      stock: 50
+    },
+    {
+      nombre: "Auriculares",
+      precio: 29.99,
+      categoria: "Electrónica",
+      stock: 200
+    },
+    {
+      nombre: "Mouse inalámbrico",
+      precio: 15.99,
+      categoria: "Electrónica",
+      stock: 150
+    },
+    {
+      nombre: "Zapatos deportivos",
+      precio: 59.99,
+      categoria: "Calzado",
+      stock: 75
+    }
+  ]);
+  
+  // Comando para verificar los datos insertados
+  db.productos.find().pretty();
+  
